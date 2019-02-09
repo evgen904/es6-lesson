839a102399d3239415c20149374ae06acae3c728
Evgen904 <evgen904@gmail.com>
2019-02-10 00:34:00 +0300

    Стрелочные функции

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
-
-
 //const q = [4,5,6]
 //console.log(q)
 
@@ -18,4 +9,48 @@ for (let i=0; i<5; i++) {
 
 const PI = 3.14;
 let a = 1;
+
+
+// const original = function () {
+//   return 100;
+// }
+//console.log(original());
+
+
+// const arrow = (num, num2) => {
+//   return 150 + num * num2;
+// }
+
+//const arrow2= (num, num2) => 150 + num * num2;
+const arrow3 = num => 150 + num;
+
+console.log(arrow3(50))
+
+
+const obj = {
+  name: 'WFM',
+
+  logName: function () {
+    // var self = this;
+    // setTimeout(function () {
+    //   console.log('Name: ' + self.name);
+    // },1000)
+
+    setTimeout(() => {
+      // Используется старый контекст
+      console.log('Name: ' + this.name);
+    },1000)
+  }
+}
+
+obj.logName();
+
+
+
+
+
+
+
+
+
 
