c531d6a183627e2324233a67f58c7fb828156e99
Evgen904 <evgen904@gmail.com>
2019-02-10 00:47:32 +0300

    Параметры по умолчанию

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 
 for (let i=0; i<5; i++) {
   setTimeout(function() {
-    console.log(i)
+    //console.log(i)
   },1000)
 }
 
@@ -24,7 +24,7 @@ let a = 1;
 //const arrow2= (num, num2) => 150 + num * num2;
 const arrow3 = num => 150 + num;
 
-console.log(arrow3(50))
+//console.log(arrow3(50))
 
 
 const obj = {
@@ -38,12 +38,27 @@ const obj = {
 
     setTimeout(() => {
       // Используется старый контекст
-      console.log('Name: ' + this.name);
+      //console.log('Name: ' + this.name);
     },1000)
   }
 }
 
 obj.logName();
+
+
+const c = 90;
+const c2 = () => 200;
+
+
+const func = (a = 20, b = a + c2()) => {
+  //undefined || 10
+  //a = a || 10;
+  return a + b;
+}
+
+//console.log(func(undefined, 80));
+console.log(func(30));
+
 
 
 
