0196ae25a874af1b17e6a5bbdda693be2bc795f4
Evgen904 <evgen904@gmail.com>
2019-02-10 00:10:32 +0300

    Переменные

--- src/main.js
+++ src/main.js
@@ -5,3 +5,17 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+
+//const q = [4,5,6]
+//console.log(q)
+
+for (let i=0; i<5; i++) {
+  setTimeout(function() {
+    console.log(i)
+  },1000)
+}
+
+const PI = 3.14;
+let a = 1;
+
